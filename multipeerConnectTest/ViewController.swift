95f3b2d5471e877efc60d5e239dc22aa3b229120
Ravian Hartono <ravian.hartono@gmail.com>
2017-11-25 05:46:15 +0700

    Initial Commit

---
+++ multipeerConnectTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multipeerConnectTest
+//
+//  Created by Christhopher Ravian Hartono on 25/11/17.
+//  Copyright © 2017 Communication iOS Foundation Batch 4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
